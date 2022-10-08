cbcc94ba7afd9b147fd8060bb963763dced75bf0
Sonechqa <sona.simonova@list.ru>
2022-10-08 10:59:01 +0300

    Init

---
+++ gulp/tasks/svgSprite.js
@@ -0,0 +1,44 @@
+const gulp = require("gulp");
+var svgSprite = require("gulp-svg-sprite"),
+  svgmin = require("gulp-svgmin"),
+  cheerio = require("gulp-cheerio"),
+  replace = require("gulp-replace");
+
+module.exports = function sprite() {
+  return gulp
+    .src("src/img/sprite/*.svg")
+    .pipe(
+      svgmin({
+        js2svg: {
+          pretty: true,
+        },
+      })
+    )
+    .pipe(
+      cheerio({
+        run: function ($) {
+          $("[fill]").removeAttr("fill");
+          $("[stroke]").removeAttr("stroke");
+          $("[style]").removeAttr("style");
+        },
+        parserOptions: { xmlMode: true },
+      })
+    )
+    .pipe(replace("&gt;", ">"))
+    .pipe(
+      svgSprite({
+        mode: {
+          symbol: {
+            sprite: "src/build/sprite.svg",
+            render: {
+              scss: {
+                dest: "src/styles/_sprite.scss",
+                template: "gulp/svg-template.scss",
+              },
+            },
+          },
+        },
+      })
+    )
+    .pipe(gulp.dest("build/"));
+};
