03eeaa76c9cccffb82d5fea4249505101464be83
Sonechqa <sona.simonova@list.ru>
2022-10-11 19:07:30 +0300

    Adaptive Slider and Cards

--- src/js/main.js
+++ src/js/main.js
@@ -7,9 +7,17 @@ const splide = new Splide(`.splide`, {
   pagination: false,
   autoplay: true,
   interval: 5000,
+  cover: true,
+  heightRatio: 0.5,
+  breakpoints: {
+    570: {
+      height: 330,
+    },
+  },
 }).mount();
 
 const sections = document.querySelectorAll(`.slider__section`);
+const titles = document.querySelectorAll(`.slider__titles`);
 
 sections.forEach((section, i) => {
   section.addEventListener(`click`, function () {
@@ -22,6 +30,11 @@ splide.on(`move`, (index) => {
     section.classList.remove(`slider__section--active`);
   });
   sections[index].classList.add(`slider__section--active`);
+
+  titles.forEach((title) => {
+    title.classList.remove(`slider__titles--active`);
+  });
+  titles[index].classList.add(`slider__titles--active`);
 });
 
 const burger = document.querySelector(`.burger`);
